e1cc76ce5df61c0516f4cc0bc02281067201f382
Edison Cayambe <edisonc_89@hotmail.com>
2023-11-07 18:57:10 -0500

    mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5EcApplication.java
@@ -0,0 +1,21 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5EcApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5EcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		System.out.println("Soy Edison Cayambe");
+	}
+
+}
